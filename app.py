2b8c74eca2c3dd1d9c891673e1935f340d527949
Nakul Pathak <nakulpathak3@hotmail.com>
2015-01-12 17:09:22 -0500

    Added UPDATE, DELETE requests as well as authentication so random peeps can't touch this

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'nakul':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
 
 tasks = [
     {
@@ -20,8 +34,9 @@ tasks = [
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks':[make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -50,9 +65,9 @@ def replace_task(task_id):
         abort(400)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != 'unicode':
+    if 'title' in request.json and type(request.json['title']) is not 'unicode':
         abort(400)
-    if 'description' in request.json and type(request.json['description']) != 'unicode':
+    if 'description' in request.json and type(request.json['description']) is not 'unicode':
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
@@ -73,6 +88,14 @@ def delete_task(task_id):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+def make_public_task(task):
+    new_task={}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 if __name__ == '__main__':
     app.run(debug=True)
